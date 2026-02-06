dc847ae825b6908634604d8695fadf3b47b4ddd5
spartan-linhtruong <linh.truong@c0x12c.com>
2026-02-06 14:39:17 +0700

    Add hello_world.py - Initial Python script

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A simple Hello World program in Python
+"""
+
+def main():
+    print("Hello, World!")
+    print("Welcome to my first Python repository!")
+
+if __name__ == "__main__":
+    main()
